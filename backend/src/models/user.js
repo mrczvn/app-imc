70b10c3f13e98caa65faf8c1b53cb363ef38d8b8
Marcos Vinicius <marvn.ants1@gmail.com>
2020-05-25 11:46:06 -0300

    feat: criado model de user

---
+++ backend/src/models/user.js
@@ -0,0 +1,31 @@
+import Sequelize, { Model, DataTypes } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        username: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+      },
+      {
+        sequelize,
+        modelName: 'User',
+        tableName: 'users',
+      }
+    );
+
+    this.addHook('beforeCreate', (user) => {
+      return user.set('password', bcrypt.hashSync(user.password, 8));
+    });
+
+    return this;
+  }
+
+  static isPassword(encodePassword, password) {
+    return bcrypt.compareSync(password, encodePassword);
+  }
+}
+
+export default User;
